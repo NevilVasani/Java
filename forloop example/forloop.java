7f3d7271b6c014e152708261de0b3abbb33cc4a0
Nevil Vasani <nevil.vasani114501@marwadiuniversity.ac.in>
2023-06-29 17:50:22 +0530

    Basic codes

---
+++ forloop example/forloop.java
@@ -0,0 +1,11 @@
+public class forloop
+{
+    public static void main(String[] args) {
+        char i;
+        for (i = 'a'; i<= 'z'; i++)
+        {
+            System.out.println(i);
+        }
+        
+    }
+}
